aba052afe97453c568fe597f054cf8dccff1ca1a
Crramos <carlos.ramosrial@usp.ceu.es>
2025-06-16 19:32:45 +0200

    update

--- enable_securityhub.py
+++ enable_securityhub.py
@@ -18,9 +18,9 @@ standards_to_enable = [
     {
         "StandardsArn": "arn:aws:securityhub:eu-west-1::standards/aws-foundational-security-best-practices/v/1.0.0"
     },
-    {
-        "StandardsArn": "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
-    },
+#    {
+#        "StandardsArn": "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0"
+#    },
     {
         "StandardsArn": "arn:aws:securityhub:eu-west-1::standards/pci-dss/v/3.2.1"
     }
